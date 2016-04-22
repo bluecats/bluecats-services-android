f0f86e71d979cebee330a8b2ee5368f7178d52fa
Henry Cheng <henry@bluecats.com>
2016-04-22 16:45:02 +1000

    throw an exception if app token is null

--- app/src/main/java/com/bluecats/services/MainApplication.java
+++ app/src/main/java/com/bluecats/services/MainApplication.java
@@ -6,16 +6,20 @@ import com.bluecats.services.interfaces.BlueCatsSDKInterfaceService;
 import android.app.Application;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 
 public class MainApplication extends Application {
 	protected static final String TAG = "MainApplication";
 	
-	private static final String BLUECATS_APP_TOKEN = "2cac72de-8100-4b19-b601-26290e98fb13";
+	private static final String BLUECATS_APP_TOKEN = "";
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
 
+		if (TextUtils.isEmpty(BLUECATS_APP_TOKEN)) {
+			throw new RuntimeException("BLUECATS_APP_TOKEN is invalid in MainApplication.java.");
+		}
 		// start the BlueCatsSDKInterfaceService
 		// this service will be responsible for handling SDK events while the app is closed
 		Intent intent = new Intent(MainApplication.this, BlueCatsSDKInterfaceService.class);
